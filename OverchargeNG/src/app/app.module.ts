6a1c4af51b51f4711bd4c61798c3abdfec168840
Tony Bahner <tony.bahner@revature.net>
2021-08-17 14:19:46 -0600

    wireframe start

---
+++ OverchargeNG/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { TimerComponent } from './components/timer/timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    TimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
