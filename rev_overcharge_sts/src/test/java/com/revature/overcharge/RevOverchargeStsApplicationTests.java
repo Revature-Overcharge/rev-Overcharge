2d8beadb4f8f1747a333e13c020224b3f3a3c517
kileader <kevin.i.leader@gmail.com>
2021-08-17 15:23:34 -0400

    Add blank spring project

---
+++ rev_overcharge_sts/src/test/java/com/revature/overcharge/RevOverchargeStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.overcharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevOverchargeStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
