c89add438e90102f3415e1e0a94d340052d91a20
Tony Bahner <tony.bahner@revature.net>
2021-08-18 15:18:44 -0600

    added foundation for log in

--- OverchargeNG/src/app/app-routing.module.ts
+++ OverchargeNG/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { TimerComponent } from './components/timer/timer.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'timer', component: TimerComponent}
+  {path: 'timer', component: TimerComponent},
+  {path: 'login', component: LoginComponent}
+
 ];
 
 @NgModule({
