8e6b71196d16908b586ffce0fc6982bfb7a5c982
jhupprevature <jordan.hupp@revature.net>
2021-08-18 14:32:23 -0400

    resolving timer merge conflicts

--- OverchargeNG/src/app/app-routing.module.ts
+++ OverchargeNG/src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './components/home/home.component';
 import { TimerComponent } from './components/timer/timer.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-=======
-import { TimerComponent } from './components/timer/timer.component';
-
-const routes: Routes = [
->>>>>>> origin/timer
   {path: 'timer', component: TimerComponent}
 ];
 
