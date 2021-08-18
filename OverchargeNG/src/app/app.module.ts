ecd91a80d6c8b093ea4211b177ef535c706e1a15
Tony Bahner <tony.bahner@revature.net>
2021-08-18 09:24:05 -0600

    pulling

--- OverchargeNG/src/app/app.module.ts
+++ OverchargeNG/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CountdownModule } from 'ngx-countdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -14,7 +15,8 @@ import { TimerComponent } from './components/timer/timer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
