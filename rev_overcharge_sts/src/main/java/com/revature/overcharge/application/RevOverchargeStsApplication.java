7e1e002a96a0adfd07cdaab01ddf4d02eb8ccf74
SeanGRev <sean.guo@revature.net>
2021-08-18 21:30:58 -0400

    Spring Annotations in Applcation

--- rev_overcharge_sts/src/main/java/com/revature/overcharge/application/RevOverchargeStsApplication.java
+++ rev_overcharge_sts/src/main/java/com/revature/overcharge/application/RevOverchargeStsApplication.java
@@ -2,8 +2,14 @@ package com.revature.overcharge.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.revature.overcharge")
+@EntityScan("com.revature.overcharge.beans")
+@EnableJpaRepositories("com.revature.overcharge.repositories")
 public class RevOverchargeStsApplication {
 
 	public static void main(String[] args) {
