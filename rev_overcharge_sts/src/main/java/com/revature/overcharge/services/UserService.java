8ed38c9bfea986767690368d7275f423573e4560
kileader <kevin.i.leader@gmail.com>
2021-08-18 23:53:45 -0400

    Add UserService with crud

---
+++ rev_overcharge_sts/src/main/java/com/revature/overcharge/services/UserService.java
@@ -0,0 +1,15 @@
+package com.revature.overcharge.services;
+
+import com.revature.overcharge.beans.User;
+
+public interface UserService {
+
+    public User addUser(User u);
+    
+    public User getUser(int id);
+    
+    public User updateUser(User newUser);
+    
+    public boolean deleteUser(int id);
+    
+}
