ca214a160f64ac95ef8a5c9e386a7770bc653716
Sean Guo <85519524+SeanGRev@users.noreply.github.com>
2021-08-20 11:53:14 -0400

    Update Deck.ts

--- OverchargeNG/src/app/components/beans/Deck.ts
+++ OverchargeNG/src/app/components/beans/Deck.ts
@@ -3,12 +3,12 @@ export class Deck {
     id: number;
     creator: User;
     title: string;
-    created_on: number;
+    createdOn: number;
 
-   constructor(id: number, creator: User, title: string, created_on: number){
+   constructor(id: number, creator: User, title: string, createdOn: number){
      this.id = id;
      this.creator = creator;
      this.title = title; 
-     this.created_on = created_on; 
+     this.createdOn = createdOn; 
   }
 }
