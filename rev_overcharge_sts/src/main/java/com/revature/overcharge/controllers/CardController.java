d04380b5ef2816a35b49efd6dfd3dc2cbea64cb2
stukruze <audiolaboratory@hotmail.com>
2021-08-20 17:14:40 -0400

    unit testing

--- rev_overcharge_sts/src/main/java/com/revature/overcharge/controllers/CardController.java
+++ rev_overcharge_sts/src/main/java/com/revature/overcharge/controllers/CardController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.overcharge.beans.Card;
+import com.revature.overcharge.beans.Deck;
 import com.revature.overcharge.services.CardService;
 
 @CrossOrigin
@@ -19,6 +22,12 @@ public class CardController {
     @GetMapping(value = "/cards/{id}")
     public Card getCard(@PathVariable("id") String id) {
         return cs.getCard(Integer.parseInt(id));
+    }
+    
+    @PostMapping(value = "/cards", consumes = "application/json",
+            produces = "application/json")
+    public Card addCard(@RequestBody Card c) {
+        return cs.addCard(c);
     }
 
 }
