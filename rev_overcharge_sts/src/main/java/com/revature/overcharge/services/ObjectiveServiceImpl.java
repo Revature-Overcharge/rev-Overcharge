e59970e4c0c0c505cfa1203e0f3c0bf8304e5af9
kileader <kevin.i.leader@gmail.com>
2021-08-23 14:58:17 -0400

    Create skeleton ObjectiveServiceImpl

---
+++ rev_overcharge_sts/src/main/java/com/revature/overcharge/services/ObjectiveServiceImpl.java
@@ -0,0 +1,36 @@
+package com.revature.overcharge.services;
+
+import java.util.List;
+
+import com.revature.overcharge.beans.Card;
+import com.revature.overcharge.beans.Deck;
+import com.revature.overcharge.beans.Objective;
+import com.revature.overcharge.beans.User;
+
+public class ObjectiveServiceImpl implements ObjectiveService {
+
+    @Override
+    public List<Objective> getAllObjectivesForUser(String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addCardObj(Card c) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addDeckObj(Deck d) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void loginObj(User user) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
