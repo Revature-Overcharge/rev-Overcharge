9d4e38e7a64bd137160826834fb9faaa79706628
DeyondreBeale <deyondre.beale@revature.com>
2021-08-27 20:25:44 -0500

    commented out a test line

--- rev_overcharge_sts/src/test/java/com/revature/overcharge/services/ObjectiveServiceTests.java
+++ rev_overcharge_sts/src/test/java/com/revature/overcharge/services/ObjectiveServiceTests.java
@@ -16,49 +16,50 @@ import com.revature.overcharge.beans.Card;
 import com.revature.overcharge.beans.Deck;
 import com.revature.overcharge.beans.User;
 
-
-@SpringBootTest(
-        classes = com.revature.overcharge.application.RevOverchargeStsApplication.class)
+@SpringBootTest(classes = com.revature.overcharge.application.RevOverchargeStsApplication.class)
 @Transactional
 public class ObjectiveServiceTests {
-	
+
 	@Autowired
-    public ObjectiveService os;
-	
+	public ObjectiveService os;
+
 	@MockBean
 	UserService rs;
-	
+
 	@MockBean
 	DeckService ds;
-    
+
 	@Test
-    void loginObjTest() {
-        
+	void loginObjTest() {
+
 		User u = new User("username", "password2", 0, 1630058559L);
-		Mockito.when(rs.addUser(u)).thenReturn(new User(1, u.getUsername(), u.getPassword(), u.getPoints(), u.getLastLogin()));
+		Mockito.when(rs.addUser(u))
+				.thenReturn(new User(1, u.getUsername(), u.getPassword(), u.getPoints(), u.getLastLogin()));
 		os.loginObj(u);
 		assertEquals(10, u.getPoints());
-    }
-	
-	@Test 
+	}
+
+	@Test
 	void setCreateADeckWeeklyTest() {
 
 		User u = new User("username2", "password3", 0, 1630058559L);
 		Card c1 = new Card("question1", "answer1", 1630058559L);
 		Card c2 = new Card("question2", "answer2", 1630058559L);
-		
+
 		List<Card> cards = Arrays.asList(c1, c2);
-		
+
 		Deck d = new Deck(u, "Deck 2", 1630058559L, cards);
-		
-		Mockito.when(rs.addUser(u)).thenReturn(new User(1, u.getUsername(), u.getPassword(), u.getPoints(), u.getLastLogin()));
-		Mockito.when(rs.getUser(u.getId())).thenReturn(new User(1, u.getUsername(), u.getPassword(), u.getPoints(), u.getLastLogin()));
-		Mockito.when(ds.addDeckAndCards(d)).thenReturn(new Deck(1, ));
+
+		Mockito.when(rs.addUser(u))
+				.thenReturn(new User(1, u.getUsername(), u.getPassword(), u.getPoints(), u.getLastLogin()));
+		Mockito.when(rs.getUser(u.getId()))
+				.thenReturn(new User(1, u.getUsername(), u.getPassword(), u.getPoints(), u.getLastLogin()));
+		// Mockito.when(ds.addDeckAndCards(d)).thenReturn(new Deck(1, ));
 //		Mockito.doThrow(ResponseStatusException.class).when(ds).getDecksByCreatorId(u.getId());
 
 		os.setCreateADeckWeekly(u.getId());
 		System.out.println(u.getObjectives());
-				
+
 	}
 
 }
